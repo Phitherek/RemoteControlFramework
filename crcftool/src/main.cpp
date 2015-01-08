6c981e88e81bc8d0f66c40240621889e34711af3
Phitherek_ <phitherek@gmail.com>
2015-01-08 17:14:53 +0100

    Client RCFTool fixed on Windows.

--- crcftool/src/main.cpp
+++ crcftool/src/main.cpp
@@ -1,11 +1,17 @@
-#include <librcfclient/librcfclient.h>
-#include <librcfcommon/librcfcommon.h>
+#include <boost/predef.h>
+#if BOOST_OS_WINDOWS
+    #define _WIN32_WINNT 0x0501
+    #define WINVER 0x0501
+#endif
 #include <iostream>
 #include <cstdlib>
 #include <string>
 #include <vector>
 #include <exception>
 #include <boost/filesystem.hpp>
+#include <boost/asio.hpp>
+#include <librcfclient/librcfclient.h>
+#include <librcfcommon/librcfcommon.h>
 using namespace std;
 
 int main(int argc, char** argv) {
